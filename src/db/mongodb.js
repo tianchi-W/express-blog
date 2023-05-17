f9aa0a174978ba9cba15f158c67067104211bb7b
tianchi-W <weitianchi001@163.com>
2023-05-17 17:36:56 +0900

    feat 连接远程服务器

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const consola = require("consola");
 
 // 连接数据库，自动新建 ExpressApi 库
-mongoose.connect("mongodb://localhost:27017/ExpressApi", {
-  //   useNewUrlParser: true, // 避免“不建议使用当前URL字符串解析器”
-  //   useCreateIndex: true, // 解决MongoDB弃用警告
-  //   useUnifiedTopology: true, // 解决连接报错问题
-  //   useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb://mongo:F0bfJhFG8lSvlK0iloWX@containers-us-west-48.railway.app:6610",
+  {
+    //   useNewUrlParser: true, // 避免“不建议使用当前URL字符串解析器”
+    //   useCreateIndex: true, // 解决MongoDB弃用警告
+    //   useUnifiedTopology: true, // 解决连接报错问题
+    //   useFindAndModify: false,
+  }
+);
 // 连接错误
 mongoose.connection.on("error", (error) => {
   consola.warn("数据库连接失败!", error);
